ebe7bd79d5c51372204818c16d8c85bcc224367e
Chrissie Akau <6412502-chrissie-akau@users.noreply.gitlab.com>
2020-07-26 15:12:17 -0700

    Project Summary Created and Nested in Project List

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (  
+        <div className="card z-depth-0 project-summary">
+               <div className="card-content grey-test text-dark-3">
+                   <span className="card-title">Project Title</span>
+                   <p>Posted by Chrissie</p>
+                   <p className="grey-text">July 25, 2020</p>
+               </div>
+           </div>
+    )
+}
+ 
+export default ProjectSummary
