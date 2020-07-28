692a4d06c3616d993d7a98cdd1457bfbd09ed742
Chrissie Akau <6412502-chrissie-akau@users.noreply.gitlab.com>
2020-07-28 00:31:32 -0700

    thunk added, projectAction added

--- src/store/reducer/projectReducer.js
+++ src/store/reducer/projectReducer.js
@@ -1,11 +1,15 @@
 const initState = {
     projects: [
-    {id: '1', title: 'help me find Peach', content: 'wear your best shoes'},
-    {id: '2', title: 'collect all the stars', content: 'jump high'},
-    {id: '3', title: 'egg hunt with Yoshi', content: 'Yoshi is the worst mom'}
+        {id: '1', title: 'help me find Peach', content: 'wear your best shoes'},
+        {id: '2', title: 'collect all the stars', content: 'jump high'},
+        {id: '3', title: 'egg hunt with Yoshi', content: 'Yoshi is the worst mom'}
     ]
 }
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+    }
     return state
 }
 
