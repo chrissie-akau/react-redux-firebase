62f9c209089c832d60130e7bb3db6bded90a5a25
Chrissie Akau <6412502-chrissie-akau@users.noreply.gitlab.com>
2020-07-27 19:58:46 -0700

    mapstatetoprops added in dashboard component, dummy data added

--- src/store/reducer/projectReducer.js
+++ src/store/reducer/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+    {id: '1', title: 'help me find Peach', content: 'wear your best shoes'},
+    {id: '2', title: 'collect all the stars', content: 'jump high'},
+    {id: '3', title: 'egg hunt with Yoshi', content: 'Yoshi is the worst mom'}
+    ]
 }
 const projectReducer = (state = initState, action) => {
     return state
