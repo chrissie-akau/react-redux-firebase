ece43ca4b4f4c1d6d1b503e26cf29a266cf44468
Chrissie Akau <6412502-chrissie-akau@users.noreply.gitlab.com>
2020-08-07 16:55:46 -0700

    Fixed i.e

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import fbConfig from './config/fbConfig'
+import firebase from './config/fbConfig'
 
 const store = createStore(rootReducer, 
   compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebase),
   )
 )
 
